d4278b60e86b8b3e45e11a4dae7f39cafe7f77dd
Alexey Savin <therealsava@icloud.com>
2021-12-16 20:13:11 +0300

    Scripted auto-commit on change (2021-12-16 20:13:11) by gitwatch.sh

--- src/main/java/ru/bmstu/messages/StoreMessage.java
+++ src/main/java/ru/bmstu/messages/StoreMessage.java
@@ -1,5 +1,19 @@
 package ru.bmstu.messages;
 
 public class StoreMessage {
+    private final String id;
+    private final String result;
+
+    public StoreMessage (String id, String result) {
+        this.id = id;
+        this.result = result;
+    }
     
+    public String getId() {
+        return id;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
