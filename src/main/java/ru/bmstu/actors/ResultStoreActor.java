1f4423b9a328554270c0127d455beb54f26eeafc
Alexey Savin <therealsava@icloud.com>
2021-12-16 19:58:10 +0300

    Scripted auto-commit on change (2021-12-16 19:58:10) by gitwatch.sh

--- src/main/java/ru/bmstu/actors/ResultStoreActor.java
+++ src/main/java/ru/bmstu/actors/ResultStoreActor.java
@@ -2,5 +2,14 @@ package ru.bmstu.actors;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ResultStoreActor extends AbstractActor {
+
+    private Map<String, ArrayList<String>> resultMap = new HashMap<>();
+
+    @Override
+    public createReceive()
+
 }
