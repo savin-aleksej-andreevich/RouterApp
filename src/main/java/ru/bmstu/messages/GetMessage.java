efa0b220eeef765ce133e216d41fdce2abe59ffb
Alexey Savin <therealsava@icloud.com>
2021-12-16 20:02:10 +0300

    Scripted auto-commit on change (2021-12-16 20:02:10) by gitwatch.sh

--- src/main/java/ru/bmstu/messages/GetMessage.java
+++ src/main/java/ru/bmstu/messages/GetMessage.java
@@ -1,4 +1,13 @@
 package ru.bmstu.messages;
 
 public class GetMessage {
+    private final String packageID;
+
+    public GetMessage (String packageID) {
+        this.packageID = packageID;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
 }
