499c3148206468baec11c5fe1f408ecc28e7b253
Alexey Savin <therealsava@icloud.com>
2021-12-16 19:54:18 +0300

    Scripted auto-commit on change (2021-12-16 19:54:18) by gitwatch.sh

--- src/main/java/ru/bmstu/actors/AppRouterActor.java
+++ src/main/java/ru/bmstu/actors/AppRouterActor.java
@@ -4,6 +4,7 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.routing.ActorRefRoutee;
+import akka.routing.RoundRobinRoutingLogic;
 import akka.routing.Routee;
 import akka.routing.Router;
 
@@ -18,12 +19,12 @@ public class AppRouterActor extends AbstractActor {
     public AppRouterActor() {
         storeActor = getContext().actorOf(Props.create(ResultStoreActor.class), "ResultStorage"); // создается ResultStorage
         List<Routee> routees = new ArrayList<>();
-        for (int i = 0; i < EXECUTORS_COUNT; i++) { //Создается 5 executor и 
+        for (int i = 0; i < EXECUTORS_COUNT; i++) { //Создается 5 executor и мониторятся
             ActorRef executor = getContext().actorOf(Props.create(ExecuteTestActor.class));
             getContext().watch(executor);
             routees.add(new ActorRefRoutee(executor));
         }
-        router
+        router = new Router(new RoundRobinRoutingLogic(), routees)
     }
 
     @Override
