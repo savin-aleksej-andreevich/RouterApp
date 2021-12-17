be8780084b9630d5227b2b6ef1fd0c5ea277e202
Alexey Savin <therealsava@icloud.com>
2021-12-17 19:10:53 +0300

    Scripted auto-commit on change (2021-12-17 19:10:53) by gitwatch.sh

--- src/main/java/ru/bmstu/messages/Test.java
+++ src/main/java/ru/bmstu/messages/Test.java
@@ -1,6 +1,8 @@
 package ru.bmstu.messages;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 
 public class Test {
     @JsonProperty("testName");
@@ -12,5 +14,28 @@ public class Test {
     @JsonProperty("params")
     private final Arraylist<Object> params;
 
+    @JsonCreator
+    public Test (
+            @JsonProperty("testName") String testName,
+            @JsonProperty("expectedResult") String expectedResult,
+            @JsonProperty("params") ArrayList<Object> params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 
+    @JsonProperty("testName");
+    public String getTestName() {
+        return testName;
+    }
+
+    @JsonProperty("expectedResult")
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    @JsonProperty("params")
+    public Arraylist<Object> getParams() {
+        return params;
+    }
 }
