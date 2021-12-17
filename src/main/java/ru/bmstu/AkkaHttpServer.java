fc2149f4fc885ea2dc3042e0cfe78dafe4d9f426
Alexey Savin <therealsava@icloud.com>
2021-12-17 19:58:49 +0300

    Scripted auto-commit on change (2021-12-17 19:58:49) by gitwatch.sh

--- src/main/java/ru/bmstu/AkkaHttpServer.java
+++ src/main/java/ru/bmstu/AkkaHttpServer.java
@@ -1,2 +1,23 @@
-package ru.bmstu;public class AkkaHttpServer {
+package ru.bmstu;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+import ru.bmstu.actors.AppRouterActor;
+
+public class AkkaHttpServer {
+    public static String SYSTEM_NAME = "appRouter";
+    public static String ROUTER_NAME = "router";
+    public static String HOST = "localhost";
+    public static int PORT = 8080;
+
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create(SYSTEM_NAME);
+        ActorRef router = system.actorOf(Props.create(AppRouterActor.class), ROUTER_NAME);
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(router);
+    }
 }
