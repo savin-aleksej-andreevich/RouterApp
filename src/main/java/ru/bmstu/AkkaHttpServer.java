baa331da420539227d95272756cb90a3c1b46bef
Alexey Savin <therealsava@icloud.com>
2021-12-17 21:02:03 +0300

    Scripted auto-commit on change (2021-12-17 21:02:02) by gitwatch.sh

--- src/main/java/ru/bmstu/AkkaHttpServer.java
+++ src/main/java/ru/bmstu/AkkaHttpServer.java
@@ -1,11 +1,18 @@
 package ru.bmstu;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import ru.bmstu.actors.AppRouterActor;
+
+import java.util.concurrent.CompletionStage;
 
 public class AkkaHttpServer {
     public static String SYSTEM_NAME = "appRouter";
@@ -19,5 +26,14 @@ public class AkkaHttpServer {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         MainHttp instance = new MainHttp(router);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> buinding = http.bindAndHandle(
+                routeFlow,
+                connectHttp.toHost(HOST, PORT),
+                materializer
+        );
+        System.out.println("Server online at http://" + HOST + ":" + PORT + "/\nPress return to stop...");
+        System.in.read();
+        buinding.thenCompose(ServerBinding::unbind).thenAccept(undound -> system.terminate());
     }
 }
