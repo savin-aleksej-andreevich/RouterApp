0842009d3b68f8cd29f0b328a4ee5ada0d5a5e34
Alexey Savin <therealsava@icloud.com>
2021-12-17 20:15:48 +0300

    Scripted auto-commit on change (2021-12-17 20:15:48) by gitwatch.sh

--- src/main/java/ru/bmstu/MainHttp.java
+++ src/main/java/ru/bmstu/MainHttp.java
@@ -1,16 +1,21 @@
 package ru.bmstu;
 
+import java.time.Duration;
+import java.util.concurrent.Future;
+
 import akka.actor.ActorRef;
 import akka.http.impl.engine.client.PoolConductor;
 import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.marshalling.Marshaller;
+import akka.http.javadsl.model.RequestEntity;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akka.util.Timeout;
-import ru.bmstu.messages.GetMessage;
-
-import java.time.Duration;
-import java.util.concurrent.Future;
 import static akka.http.javadsl.server.Directives.*;
+
+import ru.bmstu.messages.GetMessage;
+import bmstu.ru.messages.JsonMessage;
+import ru.bmstu.messages.JsonMessage;
 
 public class MainHttp {
     private final ActorRef router;
@@ -19,11 +24,18 @@ public class MainHttp {
         this.router = router;
     }
     public Route createRoute () {
-        return route {
+        return route (
             get(() -> parameter("pachageId", (id) -> {
                 Future<Object> result = Patterns.ask(router, new GetMessage(id), Timeout.create(Duration.ofSeconds(10)));
                 return CompleteOKWithFuture(result, Jackson.marshaller());
-            })
-        }
+            })),
+            post(() -> entity((Jackson.unmarshaller(JsonMessage.class), (jsonMessage) -> {
+                router.tell(jsonMessage, ActorRef.noSender());
+                return complete("Test started");
+            }))
+        );
+    }
+
+    private Route CompleteOKWithFuture(Future<Object> result, Marshaller<Object, RequestEntity> marshaller) {
     }
 }
