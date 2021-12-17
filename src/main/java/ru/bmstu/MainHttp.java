d452f5b57e59289552e945ae52a212e57a613deb
Alexey Savin <therealsava@icloud.com>
2021-12-17 20:06:18 +0300

    Scripted auto-commit on change (2021-12-17 20:06:18) by gitwatch.sh

--- src/main/java/ru/bmstu/MainHttp.java
+++ src/main/java/ru/bmstu/MainHttp.java
@@ -1,2 +1,27 @@
-package ru.bmstu;public class MainHttp {
+package ru.bmstu;
+
+import akka.actor.ActorRef;
+import akka.http.impl.engine.client.PoolConductor;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import ru.bmstu.messages.GetMessage;
+
+import java.time.Duration;
+import java.util.concurrent.Future;
+
+public class MainHttp {
+    private final ActorRef router;
+
+    public MainHttp (ActorRef router) {
+        this.router = router;
+    }
+    public Route createRoute () {
+        return route {
+            get(() -> parameter("pachageId", (id) -> {
+                Future<Object> result = Patterns.ask(router, new GetMessage(id), Timeout.create(Duration.ofSeconds(10)));
+                return CompleteOK
+            })
+        }
+    }
 }
